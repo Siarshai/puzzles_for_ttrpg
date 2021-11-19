0e53f0d49517eacb699846aeab7d3d890e19b6bc
Siarshai <searchingone37@yandex.ru>
2021-11-19 18:28:23 +0300

    Added find_words_with_common_word_suffix (extracted common implementation)

--- puzzles.py
+++ puzzles.py
@@ -12,7 +12,7 @@ from stenography.letters_before_sequence import hide_letters_before_sequence
 from stenography.nth_letter import hide_in_nth_letters
 from utils.output_formatting import write_ioi, write_doi, write_i
 from words_condition.prefixes_suffixes import find_words_with_common_word_prefix, find_words_with_common_prefix, \
-    find_words_with_common_suffix
+    find_words_with_common_suffix, find_words_with_common_word_suffix
 from utils.load_words import load_words
 from utils.preprocessing import download_freq2011, is_cache_ready, preprocess_freq2011
 from words_condition.sandwichable_words import find_double_sandwichable_words, find_sandwichable_words_multistuffing
@@ -24,6 +24,7 @@ from words_condition.word_squares import find_magic_word_squares
 registered_tasks_noinput = {
     "common_word_prefix": (find_words_with_common_word_prefix, write_ioi),
     "common_prefix": (find_words_with_common_prefix, write_ioi),
+    "common_word_suffix": (find_words_with_common_word_suffix, write_ioi),
     "common_suffix": (find_words_with_common_suffix, write_ioi),
     "double_sandwichable": (find_double_sandwichable_words, write_doi),
     "sandwichable_multistuffing": (find_sandwichable_words_multistuffing, write_doi),
--- words_condition/prefixes_suffixes.py
+++ words_condition/prefixes_suffixes.py
@@ -6,6 +6,13 @@ from itertools import chain
 from tqdm import tqdm
 
 
+def less_compare_words_backwards(word1, word2):
+    for letter1, letter2 in zip(word1[::-1], word2[::-1]):
+        if letter1 != letter2:
+            return letter1 < letter2
+    return len(word1) < len(word2)
+
+
 def find_words_with_common_part(
         eligible_words: List[str],
         extract_part: Callable[[str], str],
@@ -75,6 +82,34 @@ def find_words_with_common_suffix(
         eligible_words, lambda word: word[-suffix_size:], cluster_size_threshold)
 
 
+def find_words_with_common_word_part(
+        eligible_words: List[str],
+        eligible_part_words: List[str],
+        extract_part: Callable[[str], str],
+        compare_parts_less: Callable[[str, str], bool],
+        cluster_size_threshold: int = 4) -> List[List[str]]:
+    words_with_common_part = []
+    is_streak_started = False
+    current_streak = []
+    for word in tqdm(eligible_words, total=len(eligible_words)):
+        if is_streak_started:
+            if extract_part(word) == current_streak[0]:
+                current_streak.append(word)
+            else:
+                if len(current_streak) >= cluster_size_threshold:
+                    words_with_common_part.append(current_streak)
+                is_streak_started = False
+        while eligible_part_words and compare_parts_less(eligible_part_words[0], word):
+            eligible_part_words.pop(0)
+        if not eligible_part_words:
+            break
+        # not elif since we can end previous streak and start new one on the same time
+        if word == eligible_part_words[0]:
+            current_streak = [word]
+            is_streak_started = True
+    return words_with_common_part
+
+
 def find_words_with_common_word_prefix(
         all_words_by_class: Dict[str, List[str]],
         prefix_size: int = 4,
@@ -92,26 +127,47 @@ def find_words_with_common_word_prefix(
     ...
     """
     nouns = all_words_by_class["nouns"]
-    eligible_starting_words = sorted([word for word in nouns if len(word) == prefix_size])
     eligible_words = sorted([word for word in
                              chain.from_iterable(all_words_by_class.values())
                              if len(word) >= prefix_size])
+    eligible_starting_words = sorted([word for word in nouns if len(word) == prefix_size])
+    return find_words_with_common_word_part(
+        eligible_words,
+        eligible_starting_words,
+        lambda word: word[0:prefix_size],
+        lambda word1, word2: word1 < word2,
+        cluster_size_threshold)
 
-    words_with_common_prefix = []
-    is_streak_started = False
-    current_streak = []
-    for word in tqdm(eligible_words, total=len(eligible_words)):
-        if is_streak_started:
-            if word[0:prefix_size] == current_streak[0]:
-                current_streak.append(word)
-            else:
-                if len(current_streak) >= cluster_size_threshold:
-                    words_with_common_prefix.append(current_streak)
-                is_streak_started = False
-                eligible_starting_words.pop(0)
-        # not elif since we can end previous streak and start new one on the same time
-        if word == eligible_starting_words[0]:
-            current_streak = [word]
-            is_streak_started = True
-    return words_with_common_prefix
 
+def find_words_with_common_word_suffix(
+        all_words_by_class: Dict[str, List[str]],
+        suffix_size: int = 4,
+        cluster_size_threshold: int = 4) -> List[List[str]]:
+    """
+    Finds words ending with same suffix *which is also a valid word* of a
+    certain length (4 by default)
+    Example:
+    роба, хвороба, проба, кинопроба, фотопроба, утроба
+    лада, анфилада, баллада, услада, рулада, прохлада
+    беда, лебеда, победа, ябеда
+    ...
+    Using only nouns because verbs add too many garbage
+    """
+    nouns = all_words_by_class["nouns"]
+
+    eligible_words = sorted([word[::-1] for word in
+                             chain.from_iterable(all_words_by_class.values())
+                             if len(word) >= suffix_size])
+    eligible_words = sorted(eligible_words)
+    eligible_words = [word[::-1] for word in eligible_words]
+
+    eligible_starting_words = sorted([word[::-1] for word in nouns if len(word) == suffix_size])
+    eligible_starting_words = sorted(eligible_starting_words)
+    eligible_starting_words = [word[::-1] for word in eligible_starting_words]
+
+    return find_words_with_common_word_part(
+        eligible_words,
+        eligible_starting_words,
+        lambda word: word[-suffix_size:],
+        less_compare_words_backwards,
+        cluster_size_threshold)
