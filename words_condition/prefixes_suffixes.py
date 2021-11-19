ff38a1bbd3408833f49425328e948ddc40bec48d
Siarshai <searchingone37@yandex.ru>
2021-11-19 16:43:21 +0300

    Fixed and refactored find_words_with_common_word_prefix function, added more generic find_words_with_common_prefix function

--- words_condition/prefixes_suffixes.py
+++ words_condition/prefixes_suffixes.py
@@ -9,39 +9,84 @@ class PrefixRecord:
     current_streak: List[str]
 
 
-def find_words_with_common_word_prefix(all_words_by_class: Dict[str, List[str]]) -> List[List[str]]:
+def find_words_with_common_prefix(
+        all_words_by_class: Dict[str, List[str]],
+        prefix_size: int = 6,
+        cluster_size_threshold: int = 4) -> List[List[str]]:
     """
-    Finds words starting with same prefix which is also a valid word of a
+    Finds nouns starting with same prefix of a certain length (6 by default)
+    Example:
+    абсолют, абсолютизация, абсолютизм, абсолютность
+    абстрагирование, абстракционизм, абстракционист, абстракция
+    авантюра, авантюризм, авантюрист, авантюристка
+    ...
+    Mostly captures single-rooted words but there're a couple of interesting
+    overlaps like
+    контра, контрабанда, контрабандист, контрабас, контрабасист,
+        контрагент, контракт, контрактник, контральто,
+        контрамарка, контрапункт, контраргумент, контраст,
+        контрастность, контратака, контрацептив, контрацепция
+    Using only nouns because verbs and adjectives add too many single-rooted
+    garbage
+    """
+    eligible_words = [word for word in all_words_by_class["nouns"]
+                      if len(word) >= prefix_size]
+    eligible_words = sorted(eligible_words)
+
+    words_with_common_prefix = []
+    prefix = ""
+    current_streak = []
+    for word in eligible_words:
+        if prefix == word[0:prefix_size]:
+            current_streak.append(word)
+        else:
+            if len(current_streak) >= cluster_size_threshold:
+                words_with_common_prefix.append(current_streak)
+            prefix = word[0:prefix_size]
+            current_streak = [word]
+    if len(current_streak) >= cluster_size_threshold:
+        words_with_common_prefix.append(current_streak)
+
+    return words_with_common_prefix
+
+
+def find_words_with_common_word_prefix(
+        all_words_by_class: Dict[str, List[str]],
+        prefix_size: int = 4,
+        cluster_size_threshold: int = 4) -> List[List[str]]:
+    """
+    Finds words starting with same prefix *which is also a valid word* of a
     certain length (4 by default)
     Example:
-    авто, автобан, автобус, автоген, автомат, автор
+    авто, автобан, автобус, автоген, автомат, автор ...
         (АВТО)бан, (АВТО)бус, (АВТО)ген, (АВТО)мат, (АВТО)р
     арка, аркада, аркадия, аркан
         (АРКА)да, (АРКА)дия, (АРКА)н
-    балл, баллада, балласт, баллон
+    балл, баллада, балласт, баллон ...
         (БАЛЛ)ада, (БАЛЛ)аст, (БАЛЛ)он
     ...
     """
-    eligible_words = [word for word in chain.from_iterable(all_words_by_class.values())
-                      if 4 <= len(word) <= 7]
-    eligible_words = sorted(eligible_words)
     nouns = all_words_by_class["nouns"]
-    eligible_starting_words = {
-        3: {word for word in nouns if len(word) == 3},
-        4: {word for word in nouns if len(word) == 4},
-    }
-    n_prefix_streaks = {3: PrefixRecord("", []), 4: PrefixRecord("", [])}
-    clusters_starting_with_word = {3: [], 4: []}
-    cluster_size_threshold = 4
+    eligible_starting_words = sorted([word for word in nouns if len(word) == prefix_size])
+    eligible_words = sorted([word for word in
+                             chain.from_iterable(all_words_by_class.values())
+                             if len(word) >= prefix_size])
+
+    words_with_common_prefix = []
+    is_streak_started = False
+    current_streak = []
     for word in eligible_words:
-        for prefix_n, record in n_prefix_streaks.items():
-            if record.prefix == word[0:prefix_n]:
-                record.current_streak.append(word)
+        if is_streak_started:
+            if word[0:prefix_size] == current_streak[0]:
+                current_streak.append(word)
             else:
-                if len(record.current_streak) >= cluster_size_threshold and \
-                        record.prefix in eligible_starting_words[prefix_n]:
-                    clusters_starting_with_word[prefix_n].append(record.current_streak)
-                record.prefix = word[0:prefix_n]
-                record.current_streak = [word]
-    return clusters_starting_with_word[4]
+                if len(current_streak) >= cluster_size_threshold:
+                    words_with_common_prefix.append(current_streak)
+                is_streak_started = False
+                eligible_starting_words.pop(0)
+        # not elif since we can end previous streak and start new one on the same time
+        if word == eligible_starting_words[0]:
+            current_streak = [word]
+            is_streak_started = True
+    return words_with_common_prefix
 
