fbc988fc03af3ef0d7cfc64ce5424e32e67cc088
Siarshai <searchingone37@yandex.ru>
2021-12-01 12:57:10 +0300

    Added english variant for lettery math algorithm

--- mathy/strange_math.py
+++ mathy/strange_math.py
@@ -1,9 +1,19 @@
 from itertools import product
-from typing import List
+from typing import List, Tuple
 
 
-def lettery_math_table() -> List[str]:
+def lettery_math_table_common(numbers: List[Tuple[int, str]],
+                              operations: List[Tuple[str, str]]) -> List[str]:
     result = []
+    for opsign, opstrrepr in operations:
+        for (x, xstrrepr), (y, ystrrepr) in product(numbers, numbers):
+            strresult = len(xstrrepr) + len(opstrrepr) + len(ystrrepr)
+            line = str(x) + " " + opsign + " " + str(y) + " = " + str(strresult)
+            result.append(line)
+    return result
+
+
+def lettery_math_table_ru() -> List[str]:
     numbers = [
         (0, "ноль"),
         (1, "один"),
@@ -31,9 +41,37 @@ def lettery_math_table() -> List[str]:
         ("+", "плюс"),
         ("-", "минус"),
     ]
-    for opsign, opstrrepr in operations:
-        for (x, xstrrepr), (y, ystrrepr) in product(numbers, numbers):
-            strresult = len(xstrrepr) + len(opstrrepr) + len(ystrrepr)
-            line = str(x) + " " + opsign + " " + str(y) + " = " + str(strresult)
-            result.append(line)
+    return lettery_math_table_common(numbers, operations)
+
+
+def lettery_math_table_en() -> List[str]:
+    numbers = [
+        (0, "zero"),
+        (1, "one"),
+        (2, "two"),
+        (3, "three"),
+        (4, "four"),
+        (5, "five"),
+        (6, "six"),
+        (7, "seven"),
+        (8, "eight"),
+        (9, "nine"),
+        (10, "ten"),
+        (11, "eleven"),
+        (12, "twelve")
+    ]
+    # should there be add/subtract instead?
+    operations = [
+        ("+", "plus"),
+        ("-", "minus"),
+        ("x", "times"),
+    ]
+    return lettery_math_table_common(numbers, operations)
+
+
+def lettery_math_table() -> List[str]:
+    result = ["RU"]
+    result.extend(lettery_math_table_ru())
+    result.append("EN")
+    result.extend(lettery_math_table_en())
     return result
